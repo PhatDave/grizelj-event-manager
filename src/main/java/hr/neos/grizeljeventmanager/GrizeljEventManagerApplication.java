dc23fdcb12f8aaa2b6216d3028533609bb98d377
David Majdandžić <david.majdandzic@inovatrend.com>
2022-11-23 13:51:42 +0100

    Initial commit

---
+++ src/main/java/hr/neos/grizeljeventmanager/GrizeljEventManagerApplication.java
@@ -0,0 +1,13 @@
+package hr.neos.grizeljeventmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrizeljEventManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrizeljEventManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/neos/grizeljeventmanager/GrizeljEventManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.neos.grizeljeventmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrizeljEventManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
