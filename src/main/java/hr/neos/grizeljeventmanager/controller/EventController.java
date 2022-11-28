e94ae6d180e5b509453c1283ab18a779177a9c5f
Zvonimir Grizelj <grizelj.zvonimir@gmail.com>
2022-11-28 17:44:37 +0100

    Implement add event

--- src/main/java/hr/neos/grizeljeventmanager/controller/EventController.java
+++ src/main/java/hr/neos/grizeljeventmanager/controller/EventController.java
@@ -1,4 +1,29 @@
 package hr.neos.grizeljeventmanager.controller;
 
+import hr.neos.grizeljeventmanager.dto.request.EntryRequestDto;
+import hr.neos.grizeljeventmanager.dto.request.EventRequestDto;
+import hr.neos.grizeljeventmanager.dto.response.EntryResponseDto;
+import hr.neos.grizeljeventmanager.dto.response.EventResponseDto;
+import hr.neos.grizeljeventmanager.service.EventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
 public class EventController {
+    private final EventService eventService;
+
+    @PostMapping("/add")
+    private ResponseEntity<?> addEvent(@RequestBody EventRequestDto eventRequestDto) {
+        eventService.save(eventRequestDto);
+        return ResponseEntity.ok("ok");
+    }
+    @GetMapping("/get/{id}")
+    private ResponseEntity<?> getEvent(@PathVariable Long id) {
+        EventResponseDto eventResponseDto = eventService.getById(id);
+        return ResponseEntity.ok(eventResponseDto);
+    }
+
 }
